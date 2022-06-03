2ace22725e069b6fa7b885f1c0a714024073e267
Fabio Sinobre <engenharia.computacao4.0@gmail.com>
2022-06-02 22:43:28 -0300

    exercios de 1 a 4

---
+++ src/java_basico/com/structura/README.md
@@ -0,0 +1,81 @@
+#Exercício 01
+
+Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
+mensagem explicativa, conforme exemplos.
+
+<table>
+	<tr>
+		<td>Entrada</td><td>   Saída</td>
+	</tr>
+	<tr>
+		<td>30</td>
+	</tr>
+	<tr>
+		<td>10</td><td> soma = 407</td>
+	</tr>
+</table>
+<table>
+	<tr>
+		<td>Entrada</td><td>   Saída</td>
+	</tr>
+	<tr>
+		<td>-30</td>
+	</tr>
+	<tr>
+		<td>10</td><td> soma = -20 </td>
+	</tr>
+</table>
+<table>
+	<tr>
+		<td>Entrada</td><td>   Saída</td>
+	</tr>
+	<tr>
+		<td>0</td>
+	</tr>
+	<tr>
+		<td>0</td><td> soma = 0 </td>
+	</tr>
+</table>
+
+#Exercício 02
+Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro
+casas decimais conforme exemplos.
+Fórmula da área: area = π . r^2
+Considere o valor de π = 3.14159
+#Exercício 03
+Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
+de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+<table>
+	<tr>
+		<td>Entrada</td><td>   Saída</td>
+	</tr>
+	<tr>
+		<td>5</td>
+	</tr>
+	<tr>
+		<td>6</td><td> diferênça = -26 </td>
+	</tr>
+	<tr>
+		<td>7</td>
+	</tr>
+	<tr>
+		<td>8</td>
+	</tr>
+</table>
+<table>
+	<tr>
+		<td>Entrada</td><td>   Saída</td>
+	</tr>
+	<tr>
+		<td>5</td>
+	</tr>
+	<tr>
+		<td>6</td><td> diferênça = 86 </td>
+	</tr>
+	<tr>
+		<td>-7</td>
+	</tr>
+	<tr>
+		<td>8</td>
+	</tr>
+</table>
--- src/java_basico/com/structura/Structure.java
+++ src/java_basico/com/structura/Structure.java
@@ -1,9 +1,64 @@
 package java_basico.com.structura;
 
+import java.util.Scanner;
+
 public class Structure {
 
 	public static void main(String[] args) {
-		System.out.println("Test de carregamento no git!");
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b, c, d, sum, difference, ra;
+		double workHours;
+		double valueHours;
+		double salary;
+		double PI = 3.14159;
+		double raio;
+		double area;
+			
+		System.out.println("Digite um número de 1 a dez para escolher a formula");
+		int number = scan.nextInt();
+		switch(number) {
+		case 1:
+			System.out.println("Digite o primeiro número: ");
+			a = scan.nextInt();
+			System.out.println("Digite o segundo número para realizar a soma: ");
+			b = scan.nextInt();
+			sum = a+b;
+			System.out.println("A soma de A + B é: " + sum);
+			break;
+		case 2:
+			System.out.println("Digite o raio para cálcular a área do círculo: ");
+			raio = scan.nextDouble();
+			area = 2*PI*raio*raio;
+			System.out.printf("A área é de: %.2f", area);
+			break;
+		case 3:
+			System.out.println("Digite o primeiro número: ");
+			a = scan.nextInt();
+			System.out.println("Digite o segundo número: ");
+			b = scan.nextInt();
+			System.out.println("Digite o terceiro número: ");
+			c = scan.nextInt();
+			System.out.println("Digite o quarto número: ");
+			d = scan.nextInt();
+			difference = a*b-c*d;
+			System.out.println("A diferença entre os 4 números é: " + difference);
+			break;
+		case 4:
+			System.out.println("Digite o RA do funcionário: ");
+			ra = scan.nextInt();
+			System.out.println("Quantidade de horas que o funcionário trabalhor: ");
+			workHours = scan.nextDouble();
+			System.out.println("Valor por hora: ");
+			valueHours = scan.nextDouble();
+			salary = workHours * valueHours;
+			System.out.println("número do funcionário = " + ra);
+			System.out.printf("Salário = R$ %.2f", salary);
+			break;
+			
+		}
+		
+		
 	}
 
 }
